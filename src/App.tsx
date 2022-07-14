47c0ad3422daa36e7ab5778c52ad56a9d747ae67
Ponyhook <leon.buntis@gmail.com>
2022-07-15 01:26:30 +0300

    add some init material design

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import CarService, { User } from './services/CarService';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import { Avatar, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Paper } from '@mui/material';
+import React from 'react';
 
 function App() {
 
@@ -23,22 +24,62 @@ function App() {
 
 
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
-        </Typography>
-        <div className="App">
-          {users.map(user =>
-            <div key={user.userid}>
-              <p>{user.owner.name} {user.owner.surname}</p>
-              <ul>
-                {user.vehicles.map(vehicle => <li key={vehicle.vin}>{vehicle.model} {vehicle.make}</li>)}
-              </ul>
-            </div>
-          )}
-        </div>
-      </Box>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              {users.map(user =>
+                <>
+                  <ListItem alignItems="flex-start" key={user.userid}>
+                    <ListItemAvatar>
+                      <Avatar
+                        alt={`${user.owner.name} ${user.owner.surname}`}
+                        src={user.owner.foto}
+                      />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={`${user.owner.name} ${user.owner.surname}`}
+                      secondary={
+                        <React.Fragment>
+                          <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                          >
+                            Ali Connors
+                          </Typography>
+                          {" — I'll be in your neighborhood doing errands this…"}
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                </>
+              )}
+            </List>
+            {/* {users.map(user =>
+              <div key={user.userid}>
+                <Avatar
+                  alt={`${user.owner.name} ${user.owner.surname}`}
+                  src={user.owner.foto}
+                  sx={{ width: 56, height: 56 }} />
+                <p>{user.owner.name} {user.owner.surname}</p>
+                <ul>
+                  {user.vehicles.map(vehicle => <li key={vehicle.vin}>{vehicle.model} {vehicle.make}</li>)}
+                </ul>
+              </div>
+            )} */}
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -11,16 +11,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import DashboardContent from './layout/Dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <DashboardContent>
       <App />
-    </ThemeProvider>
+    </DashboardContent>
   </React.StrictMode>
 );
 
