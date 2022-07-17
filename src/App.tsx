4b96944008f3fb9a3fa4f09833c19f64662b7f12
Ponyhook <leon.buntis@gmail.com>
2022-07-17 15:05:17 +0300

    add simple list view with interactions

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={8} lg={9}>
+        <Grid item xs={12} md={6} lg={6}>
           <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', }}>
             <UserList />
           </Paper>
--- src/pages/Users/UserList.tsx
+++ src/pages/Users/UserList.tsx
@@ -1,34 +1,90 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText, Typography, Divider, List } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import CarService, { User } from "../../services/CarService";
+import { StarBorder } from "@mui/icons-material";
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Typography, Divider, List, ListItemButton, Collapse, ListItemIcon } from "@mui/material";
+import React, { useState, useEffect, useReducer } from "react";
+import CarService, { User, Vehicle } from "../../services/CarService";
+import { GetColorName } from 'hex-color-to-color-name';
 
-const UserComponent = ({ user }: { user: User }): JSX.Element => {
+const VehicleComponent = ({ vehicle }: { vehicle: Vehicle }) => {
+    const displayCoordinates = (vehicle: Vehicle) => () => {
+        console.log(vehicle);
+    }
+
     return (
         <>
             <ListItem alignItems="flex-start" >
-                <ListItemAvatar>
-                    <Avatar
-                        alt={`${user.owner.name} ${user.owner.surname}`}
-                        src={user.owner.foto}
+                <ListItemButton onClick={displayCoordinates(vehicle)}>
+                    <ListItemAvatar>
+                        <Avatar
+                            alt={`${vehicle.make} ${vehicle.model}`}
+                            src={vehicle.foto}
+                        />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={`${vehicle.make} ${vehicle.model} (${GetColorName(vehicle.color)})`}
+                        secondary={
+                            <>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    {vehicle.year}
+                                </Typography>
+                                {` — ${vehicle.vin}`}
+                            </>
+                        }
                     />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={`${user.owner.name} ${user.owner.surname}`}
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline' }}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                Ali Connors
-                            </Typography>
-                            {" — I'll be in your neighborhood doing errands this…"}
-                        </React.Fragment>
-                    }
-                />
+                </ListItemButton>
             </ListItem>
+            <Divider variant="inset" component="li" />
+        </>
+    );
+}
+
+const VehicleList = ({ initVehicles }: { initVehicles: Vehicle[] }) => {
+    const [vehicles, setVehicles] = useState(initVehicles);
+
+    return vehicles.map(vehicle => <VehicleComponent vehicle={vehicle} />);
+}
+
+const UserComponent = ({ user }: { user: User }): JSX.Element => {
+
+    // const [vehicles, setVehicles] = useState<Vehicle[] | []>([]);
+
+    const [showVehicles, toggleVehicles] = useReducer(prev => !prev, false);
+
+    return (
+        <>
+            <ListItem alignItems="flex-start" >
+                <ListItemButton onClick={toggleVehicles}>
+                    <ListItemAvatar>
+                        <Avatar
+                            alt={`${user.owner.name} ${user.owner.surname}`}
+                            src={user.owner.foto}
+                        />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={`${user.owner.name} ${user.owner.surname} (${user.vehicles.length})`}
+                        // secondary={
+                        //     <>
+                        //         <Typography
+                        //             sx={{ display: 'inline' }}
+                        //             component="span"
+                        //             variant="body2"
+                        //             color="text.primary"
+                        //         >
+                        //             Vehicles ({user.vehicles.length})
+                        //         </Typography>
+                        //         {" — click to view vehicles"}
+                        //     </>
+                        // }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <Collapse in={showVehicles} timeout="auto" unmountOnExit>
+                {user.vehicles.map(vehicle => <VehicleComponent vehicle={vehicle} key={vehicle.vehicleid} />)}
+            </Collapse>
             <Divider variant="inset" component="li" />
         </>
     );
