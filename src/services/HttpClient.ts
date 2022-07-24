c962c3b777e2ce36fcd1f9253f7520bdd49a3274
Ponyhook <leon.buntis@gmail.com>
2022-07-24 16:17:31 +0300

    add local storage for cache

--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -1,4 +1,9 @@
 import Axios from 'axios';
-import { setupCache } from 'axios-cache-interceptor';
+import { buildWebStorage, setupCache } from 'axios-cache-interceptor';
 
-export const HttpClient = setupCache(Axios, { ttl: 1000 * 60 * 5 });
+const cacheStorage = buildWebStorage(sessionStorage, 'axios-cache:');
+
+export const HttpClient = setupCache(Axios, { 
+    ttl: 1000 * 60 * 5,
+    storage: cacheStorage
+ });
